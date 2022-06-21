5aafd2ccdd7469ef9391075061a445da83ce58e6
LizEwert <ewert@bzz.ch>
2022-06-21 16:22:14 +0200

    Projekt fertig

--- script/server/server.js
+++ script/server/server.js
@@ -1,15 +1,19 @@
 //email regex, um zu wissen wie eine email validiert wird
-const emailregex = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/
+const emailregex = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/
+const fs = require('fs');
 //modul benutzt express
 let express = require("express");
 let app     = express();
-const port = process.env.PORT || 3000;
+const port  = process.env.PORT || 3000;
 //bodyparser modul wird benutzt
 const bodyparser = require("body-parser")
 const server = app.listen(port);
 console.log(`Running at Port ${port}`);
 server.timeout = 1000 * 60 * 2; // 2 minutes
-let data = [];
+let data = fs.readFileSync('script/server/data.json', { encoding: "utf8", flag: "r" });
+if (data.length) {
+    data = JSON.parse(data);
+}
 //body parser
 // app.use(bodyparser.urlencoded())
 app.use(bodyparser.urlencoded({extended:true}))
@@ -20,22 +24,26 @@ app.use(function (req, res, next) {
     res.header('Content-Type', 'application/json');
     next();
 });
-app.get('/data', (req, res) => {
-    res.send(data)
-})
+//data speichern
+function saveData(data) {
+    fs.writeFile('script/server/data.json', data,  { encoding: "utf8", flag: "w" }, (err) => {
+        if(err){
+            throw err;
+        }
+        console.log("JSON daten sind gespeichert");
+    });
+}
 //Das kommt bei Postman als info für mich raus
 //nur unter validate (dem Pfad) wird die Funktion ausgeführt
 app.post("/validate",(req,res)=>{
     const {radio, vorname, nachname, email, dropdown, agb} = req.body;
     const errors = {};
-
     //Validierung
     if (
         !vorname || vorname.length<2 || vorname.length>10
     ){
         errors["vorname"] = "Vorname nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("Vorname nicht korrekt")
     }
     //wenn Validierung okay ist
     if (
@@ -43,56 +51,38 @@ app.post("/validate",(req,res)=>{
     ){
         errors["radio"] = "radio nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("radio nicht korrekt")
     }
     if (
         !nachname || nachname.length<2 || nachname.length>15
     ){
         errors["nachname"] = "nachname nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("Nachname nicht korrekt")
     }
     if (
         !email || !emailregex.test(email)
     ){
         errors["email"] = "email nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("E-Mail nicht korrekt")
     }
     if (
         !dropdown
     ){
         errors["dropdown"] = "dropdown nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("Kein Produkt ausgewählt")
     }
     if (
         !agb || agb === "1"
     ){
         errors["agb"] = "agb nicht korrekt";
         //wenn Validierung fehlschlägt
-        // res.status(400).send("AGB nicht akzeptiert")
     }
     if (Object.keys(errors).length === 0){
-        data = [
-            ...data,
-            {
-                radio,
-                vorname,
-                nachname,
-                email,
-                dropdown
-            }
-        ]
-        console.log(data)
-        res.send("alles ist richtig")
+        data = [...data, { radio, vorname, nachname, email, dropdown }];
+        saveData(JSON.stringify(data));
+        res.status(200).send("alles ist richtig");
     } else {
         console.error(errors);
         res.status(400).send(errors);
     }
 })
-//redirecting
-function confirmInput() {
-    fname = document.forms['vorname'].fname.value;
-    alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
-}
+
--- script/validate-inputs.js
+++ script/validate-inputs.js
@@ -5,36 +5,33 @@ const nachname = document.getElementById('nachname');
 const mail = document.getElementById('email');
 const haken= document.forms["form"]["agb"];
 const flavour = document.getElementById('dropdown');
-const info = document.getElementsByClassName("form-check-inline");
-//const gender = document.forms["form"]["fieldset"];
 const genDiv = document.getElementById("diverse");
 const genMale = document.getElementById("male");
 const genFemale = document.getElementById("female");
 const genderField = document.getElementById("geschlecht");
-
-
+var x = "true"
 //Error anzeigen
 function showError(input, message){
     const formGroup = input.parentElement;
     formGroup.className = 'form-group error';
     const small = formGroup.querySelector('small');
     small.innerText = message;
+    x ="false";
 }
-
 //Erfolg anzeigen
 function showSuccess(input, message){
     const formGroup = input.parentElement;
     formGroup.className = 'form-group success';
     const small = formGroup.querySelector('small');
     small.innerText = message;
+    x = "true";
 }
-
 // pflicht Felder checken
 function checkRequired(inputArr){
     let isRequired = false;
     inputArr.forEach(function(input) {
         if (input.value.trim() === '') {
-            //error
+//error
             showError(input, `${getFieldName(input)} ist pflicht`);
             isRequired = false;
         } else {
@@ -43,7 +40,6 @@ function checkRequired(inputArr){
     })
     return isRequired;
 }
-
 //dropdown
 function getDropdown(input){
     if (input.value === ''){
@@ -52,8 +48,6 @@ function getDropdown(input){
         showSuccess(input, 'Danke für Deine Rückmeldung!');
     }
 }
-
-
 //gender überprüfung
 function getGender(div,male,female,gender){
     if (div.checked === false && male.checked === false && female.checked === false){
@@ -62,8 +56,6 @@ function getGender(div,male,female,gender){
         showSuccess(gender, '');
     }
 }
-
-
 //checkbox überprüfung
 function getChecked(){
     const checkbox = haken.checked;
@@ -73,7 +65,6 @@ function getChecked(){
         showSuccess(haken,'');
     }
 }
-
 //email überprüfen
 function checkMail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -83,7 +74,6 @@ function checkMail(input) {
         showError(input, 'Email ist nicht gültig');
     }
 }
-
 // Prüfung der Länge der Eingabe
 function checkLength(input, min, max){
     if(input.value.length < min){
@@ -96,31 +86,30 @@ function checkLength(input, min, max){
         showSuccess(input,'');
     }
 }
-
 //Feldname gross
 function getFieldName(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
-
-
-
-
 //Formular validierung der Eingaben
 function validateForm(){
     if (!checkRequired([vorname, nachname, mail])){
         //success
+        getChecked();
         checkLength(vorname,2,10);
         checkLength(nachname, 2,20);
         checkMail(mail);
-        getChecked();
         getDropdown(flavour);
-        //getGender(info);
         getGender(genDiv,genMale,genFemale,genderField);
+        showPage();
     }
 }
-
 // Event listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
     validateForm();
 });
+function showPage(){
+    if (x === "true"){
+        alert ("Danke für deine Anmeldung!");
+    }
+}
