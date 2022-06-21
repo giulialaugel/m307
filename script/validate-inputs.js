32628390660670fdb26b7964e6cd339d8f3bbc7b
Giulia Laugel <laugelg@bzz.ch>
2022-06-21 09:09:35 +0200

    validierung gemacht

--- script/validate-inputs.js
+++ script/validate-inputs.js
@@ -3,10 +3,14 @@ const form = document.getElementById('form');
 const vorname = document.getElementById('vorname');
 const nachname = document.getElementById('nachname');
 const mail = document.getElementById('email');
-const haken = document.getElementById('agb');
-const dropdown = document.getElementById('dropdown');
-const user = dropdown.options[dropdown.selectedIndex].value;
-const radio = document.getElementById('gender');
+const haken= document.forms["form"]["agb"];
+const flavour = document.getElementById('dropdown');
+const info = document.getElementsByClassName("form-check-inline");
+//const gender = document.forms["form"]["fieldset"];
+const genDiv = document.getElementById("diverse");
+const genMale = document.getElementById("male");
+const genFemale = document.getElementById("female");
+const genderField = document.getElementById("geschlecht");
 
 
 //Error anzeigen
@@ -18,9 +22,11 @@ function showError(input, message){
 }
 
 //Erfolg anzeigen
-function showSuccess(input){
+function showSuccess(input, message){
     const formGroup = input.parentElement;
     formGroup.className = 'form-group success';
+    const small = formGroup.querySelector('small');
+    small.innerText = message;
 }
 
 // pflicht Felder checken
@@ -30,57 +36,48 @@ function checkRequired(inputArr){
         if (input.value.trim() === '') {
             //error
             showError(input, `${getFieldName(input)} ist pflicht`);
-            isRequired = true;
+            isRequired = false;
         } else {
-            showSuccess(input);
+            showSuccess(input,'');
         }
     })
     return isRequired;
 }
 
-//Formular validierung der Eingaben
-function validateForm(){
-    if (!checkRequired([vorname, nachname, mail, haken,dropdown, radio])){
-        //success
-        checkLength(vorname,2,10);
-        checkLength(nachname, 2,20);
-        checkMail(mail);
-        getDropdown(dropdown);
-        getChecked(haken);
-        getGender(radio);
-    }
-}
-
-//checkbox überprüfen
-function getChecked(input){
-    if (document.getElementById('agb').checked) {
-        console.log("no");
-        showError(input, 'Setze einen Haken');
-    }else {
-        console.log("yes");
-        showSuccess(input);
-    }
-}
-
-//geschlecht überprüfen
-function getGender(input){
-    if (document.getElementById('Divers').checked){
-        showSuccess(input);
-    }else if(document.getElementById('Frau').checked){
-        showSuccess(input);
-    }else if(document.getElementById('Mann').checked){
-        showSuccess(input);
-    } else {
-    showError(input, 'Bitte wähle eins aus');
-    }
-}
-
-//dropdown überprüfen
+//dropdown
 function getDropdown(input){
-    if(user === 0){
-        showError(input);
-    } else if (user >= 0){
-        showSuccess(input);
+    if (input.value === ''){
+        showError(input, 'Wähle eins aus!');
+    }else if (input.value > 0){
+        showSuccess(input, 'Danke für deine Rückmeldung!');
+    }
+}
+
+
+//gender überprüfung
+/*function getGender(input) {
+    if (input.value === ''){
+        showError(gender, 'bitte wähle eins an');
+    } else {
+        showSuccess(gender, '');
+    }
+}*/
+function getGender(div,male,female,gender){
+    if (div.checked === false && male.checked === false && female.checked === false){
+        showError(gender,'bitte wähle eins an');
+    } else {
+        showSuccess(gender, '');
+    }
+}
+
+
+//checkbox überprüfung
+function getChecked(){
+    const checkbox = haken.checked;
+    if(checkbox === false) {
+        showError(haken, 'Bitte kreuze an');
+    }else if(checkbox=== true) {
+        showSuccess(haken,'');
     }
 }
 
@@ -88,7 +85,7 @@ function getDropdown(input){
 function checkMail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
-        showSuccess(input);
+        showSuccess(input,'');
     } else {
         showError(input, 'Email ist nicht gültig');
     }
@@ -103,13 +100,30 @@ function checkLength(input, min, max){
         showError(input,
             `${getFieldName(input)} muss weniger als ${max} Buchstaben beinhalten`);
     } else{
-        showSuccess(input);
+        showSuccess(input,'');
     }
 }
 
 //Feldname gross
 function getFieldName(input){
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+
+
+//Formular validierung der Eingaben
+function validateForm(){
+    if (!checkRequired([vorname, nachname, mail])){
+        //success
+        checkLength(vorname,2,10);
+        checkLength(nachname, 2,20);
+        checkMail(mail);
+        getChecked();
+        getDropdown(flavour);
+        //getGender(info);
+        getGender(genDiv,genMale,genFemale,genderField);
+    }
 }
 
 // Event listeners
